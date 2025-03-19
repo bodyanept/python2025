58eae3a7bd7cde5145d268e8dbf06d8715a09522
bodyanept <115324255+bodyanept@users.noreply.github.com>
2025-03-19 21:12:01 +0500

    Update check_input.py

--- students/Krasilnikov B/check_input.py
+++ students/Krasilnikov B/check_input.py
@@ -1,13 +1,25 @@
-# for letter in user_string:
-#     if letter.isspace
 try:
     user_string = input('введи какую нибудь строку')
+    for letter in user_string:
+        if not(letter.isalpha() and letter.isascii()) and letter !=' ':
+            raise ValueError('Ошибка! В этой строке не должно быть ничего кроме латинских букв и пробелов!!!')
 except ValueError:
     print('Ошибка! В этой строке не должно быть ничего кроме букв!')
 else:
-    print(user_string.upper())
+    print(f'Преобрахованная строка: {user_string.upper()}')
     try:
-        n = int(input('введи индекс своей строки'))
-        print(user_string[n])
-    except IndexError:
-        print('Вы ввели не существующий индекс!!!')
+        n = input('введи индекс своей строки')
+        if not n.isdigit():
+            raise TypeError('Индекс должен быть целым числом.')
+    except TypeError as e:
+        print(f'Ошибка: {e}')
+    else:
+        try:
+            i = int(n)
+            print(f'символ на позиции {i}: {user_string[i]}')
+        except IndexError:
+            print('Вы ввели не существующий индекс!!!')
+
+
+
+
